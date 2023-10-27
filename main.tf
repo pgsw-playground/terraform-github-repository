eac7d23aa70ad187a59f7a2b05f24279a85216d0
Przemysław Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2023-10-27 13:36:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,4 +3,10 @@ resource "github_repository" "example" {
   description = "Demo"
 
   visibility = "public"
+
+  template {
+    owner                = "pgsw-pogosoftware"
+    repository           = "terraform-github-devops-repository-template"
+    include_all_branches = false
+  }
 }
