8c57923a8d07cc1a6adb10b5a1663f2a61c5108b
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2023-10-27 17:33:02 +0200

    Set review count to 0

--- main.tf
+++ main.tf
@@ -48,7 +48,8 @@ resource "github_branch_protection" "this" {
   require_conversation_resolution = true
 
   required_pull_request_reviews {
-    dismiss_stale_reviews      = true
-    require_code_owner_reviews = true
+    required_approving_review_count = 0
+    dismiss_stale_reviews           = true
+    require_code_owner_reviews      = true
   }
 }
